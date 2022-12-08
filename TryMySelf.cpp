52a8cbd94f1feefa618866f01a038542e9d2a9fc
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-12-08 20:57:11 +0530

    Add files via upload

---
+++ TryMySelf.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->right = NULL;
+        this->left = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the Data" << endl;
+
+    int data;
+    cin >> data;
+
+    root = new node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter the data in Left  " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of  " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            // purana level complete traverse ho chuka hai
+            cout << endl;
+            if (!q.empty())
+            {
+                // queue still has some child ndoes
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+
+    node *root = NULL;
+    buildTree(root);
+    levelOrderTraversal(root);
+}
